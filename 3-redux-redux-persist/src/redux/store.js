9ff440f94cc6794cf2e4307e0fb3fb9c59235052
Ha Le <halntit@gmail.com>
2023-09-17 23:05:09 +0700

    redux persist finalize

--- 3-redux-redux-persist/src/redux/store.js
+++ 3-redux-redux-persist/src/redux/store.js
@@ -1,4 +1,6 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { counterReducer as countReducer } from './countReducer';
 import { todosReducer } from './todosReducer';
 import thunk from 'redux-thunk';
@@ -10,9 +12,18 @@ const loggingMiddleware = (store) => (next) => (action) => {
   next(action);
 }
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['count'],
+  blacklist: ['todos'],
+};
+
 const rootReducer = combineReducers({
   count: countReducer,
   todos: todosReducer
 });
 
-export const store = createStore(rootReducer, applyMiddleware(loggingMiddleware, thunk));
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const store = createStore(persistedReducer, applyMiddleware(loggingMiddleware, thunk));
+export const persistor = persistStore(store);
